f0891c61e4897bb3261a6665bc605f0e53b6499c
Swarnab Garang <swarnabgarang@gmail.com>
2021-07-11 13:26:47 +0530

    added styles to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,45 @@
 import React from "react";
+import "./Login.css";
 
 function Login() {
     return (
         <div className="Login">
-            <div className="login__image">
-                <img src="https://foodict.s3.ap-south-1.amazonaws.com/background.jpg"></img>
+            <div className="login__imagecontainer">
+                <img
+                    className="login__image"
+                    src="https://foodict.s3.ap-south-1.amazonaws.com/background.jpg"
+                    alt=""
+                ></img>
             </div>
             <div className="login__auth">
-                <div className="logo">
-                    <img src="https://foodict.s3.ap-south-1.amazonaws.com/foodictLogo.png"></img>
+                <div>
+                    <img
+                        className="login__logo"
+                        src="https://foodict.s3.ap-south-1.amazonaws.com/foodictLogo.png"
+                        alt=""
+                    ></img>
                 </div>
-                <div className="login__form">
-                    <form>
-                        <label>Username</label>
-                        <input type="text" placeholder="Username"></input>
-                        <label>Password</label>
-                        <input type="password" placeholder="Password"></input>
+                <div className="login__nonimage">
+                    <form className="login__form">
+                        <div className="login__forminputs">
+                            <input
+                                type="text"
+                                placeholder="Username"
+                                className="login__form__inputfield"
+                            ></input>
+                        </div>
+                        <div className="login__forminputs">
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className="login__form__inputfield"
+                            ></input>
+                        </div>
+                        <button className="login__form__button">Sign In</button>
                     </form>
+                    <p className="register__text">
+                        Don't Have an account with us? <a href="#">Register</a>
+                    </p>
                 </div>
             </div>
         </div>
