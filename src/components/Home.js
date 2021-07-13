350c4613975abd725c3e868d9371af5c45e25423
Swarnab Garang <swarnabgarang@gmail.com>
2021-07-13 20:21:17 +0530

    styled the search part of the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,25 @@
 import React from "react";
+import "./Home.css";
+
+import SearchIcon from "@material-ui/icons/Search";
 
 function Home() {
     return (
         <div className="Home">
             <div className="search__container">
-                <div className="search__heading">
-                    <h2 className="search__text">
-                        What are you craving today?
-                    </h2>
-                </div>
+                <h2 className="search__text">
+                    What are you{" "}
+                    <span className="craving__orange">craving </span>
+                    today?
+                </h2>
                 <div className="search__input">
-                    <input type="text" className="search__inputfienld"></input>
-                    <button>Search</button>
+                    <input type="text" className="search__inputfield"></input>
+                    <button className="search__button">
+                        <SearchIcon
+                            className="button_icon"
+                            style={{ fontSize: 22 }}
+                        />
+                    </button>
                 </div>
             </div>
         </div>
