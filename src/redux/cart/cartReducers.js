4044db7d1213d748659cfd8c08c2cfe0d92b72b5
Swarnab Garang <swarnabgarang@gmail.com>
2021-07-21 19:56:27 +0530

    added cartReducer in ./redux

---
+++ src/redux/cart/cartReducers.js
@@ -0,0 +1,17 @@
+const initialState = {
+    numOfItems = 0
+}
+
+const cartReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'ADD_TO_CART':
+            return {
+                ...state,
+                numOfItems: state.numOfItems + 1
+            }
+            default:
+                return state
+    }
+}
+
+export default cartReducer
