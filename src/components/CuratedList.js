03def02fb5fac1a846bcfa411eb0bf98bf8f5d41
Swarnab Garang <swarnabgarang@gmail.com>
2021-08-09 21:20:45 +0530

    added disabled add to cart button if item in cart

--- src/components/CuratedList.js
+++ src/components/CuratedList.js
@@ -1,33 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import Snackbar from "@material-ui/core/Snackbar";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
-
-import { addToCart } from "../redux";
 
 import "./CuratedList.css";
+import CuratedListItems from "./CuratedListItems";
 
 function CuratedList({ pretext, keytext, posttext, id }) {
     const [curatedList, setCuratedList] = useState([]);
-    const [open, setOpen] = useState(false);
-    const [itemName, setItemName] = useState("");
-
-    const handleClick = (name) => {
-        setOpen(true);
-        setItemName(name);
-    };
-
-    const handleClose = (event, reason) => {
-        if (reason === "clickaway") {
-            return;
-        }
-
-        setOpen(false);
-    };
-
-    const dispatch = useDispatch();
 
     useEffect(() => {
         const eatery = { restaurant: id };
@@ -69,60 +47,7 @@ function CuratedList({ pretext, keytext, posttext, id }) {
                 </span>
                 <div className="curated__listitems" ref={ref}>
                     {curatedList.map((item) => (
-                        <div className="individual__item" key={item._id}>
-                            <div className="image__container">
-                                <img
-                                    src={item.item_imageurl}
-                                    alt="food img"
-                                    className="item__image"
-                                ></img>
-                            </div>
-                            <div className="item__description">
-                                <div className="item__name">
-                                    {item.item_name}
-                                </div>
-                                <div className="item__res_dist">
-                                    <span className="item__restaurant">
-                                        {item.res_name}
-                                    </span>
-                                    <span className="item__distance">5 Km</span>
-                                    <span className="item__price">
-                                        ₹ {item.price}
-                                    </span>
-                                </div>
-                            </div>
-                            <button
-                                className="addcart__button"
-                                onClick={() => {
-                                    dispatch(addToCart(item));
-                                    handleClick(item.item_name);
-                                }}
-                            >
-                                Add to Cart
-                            </button>
-                            <Snackbar
-                                anchorOrigin={{
-                                    vertical: "bottom",
-                                    horizontal: "left",
-                                }}
-                                open={open}
-                                autoHideDuration={1500}
-                                onClose={handleClose}
-                                message={`${itemName} was added to Cart`}
-                                action={
-                                    <React.Fragment>
-                                        <IconButton
-                                            size="small"
-                                            aria-label="close"
-                                            color="inherit"
-                                            onClick={handleClose}
-                                        >
-                                            <CloseIcon fontSize="small" />
-                                        </IconButton>
-                                    </React.Fragment>
-                                }
-                            />
-                        </div>
+                        <CuratedListItems item={item} />
                     ))}
                     <span
                         className="angleRight sliderButton"
---
+++ src/components/CuratedListItems.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+
+import { addToCart } from "../redux";
+
+function CuratedListContainer({ item }) {
+    const [open, setOpen] = useState(false);
+    const [itemName, setItemName] = useState("");
+    const [addedCart, setAddedCart] = useState(false);
+
+    const items = useSelector((state) => state.items);
+
+    useEffect(() => {
+        items.map((cartItem) => {
+            if (cartItem._id == item._id) {
+                setAddedCart(true);
+            }
+        });
+    }, [items]);
+
+    const handleClick = (name) => {
+        setOpen(true);
+        setItemName(name);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <div className="individual__item" key={item._id}>
+                <div className="image__container">
+                    <img
+                        src={item.item_imageurl}
+                        alt="food img"
+                        className="item__image"
+                    ></img>
+                </div>
+                <div className="item__description">
+                    <div className="item__name">{item.item_name}</div>
+                    <div className="item__res_dist">
+                        <span className="item__restaurant">
+                            {item.res_name}
+                        </span>
+                        <span className="item__distance">5 Km</span>
+                        <span className="item__price">₹ {item.price}</span>
+                    </div>
+                </div>
+                <button
+                    className="addcart__button"
+                    onClick={() => {
+                        dispatch(addToCart(item));
+                        handleClick(item.item_name);
+                    }}
+                    disabled={addedCart}
+                >
+                    Add to Cart
+                </button>
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "left",
+                    }}
+                    open={open}
+                    autoHideDuration={1500}
+                    onClose={handleClose}
+                    message={`${itemName} was added to Cart`}
+                    action={
+                        <React.Fragment>
+                            <IconButton
+                                size="small"
+                                aria-label="close"
+                                color="inherit"
+                                onClick={handleClose}
+                            >
+                                <CloseIcon fontSize="small" />
+                            </IconButton>
+                        </React.Fragment>
+                    }
+                />
+            </div>
+        </div>
+    );
+}
+
+export default CuratedListContainer;
